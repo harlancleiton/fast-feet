c9ecf6caab429e7899f997dbf335bc641be55b57
Harlan Cleiton <harlancleiton@gmail.com>
2020-02-07 00:08:53 -0300

    chore(server): add user model

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Harlan Cleiton',
+    email: 'harlancleiton@gmail.com',
+    password: '123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
