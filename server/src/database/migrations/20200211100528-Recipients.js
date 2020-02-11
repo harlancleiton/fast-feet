365bc06f59f11d6cefb4adac80559e23042c9dcd
Harlan Cleiton <harlancleiton@gmail.com>
2020-02-11 08:03:46 -0300

    chore(server): add model and recipient migration

---
+++ server/src/database/migrations/20200211100528-Recipients.js
@@ -0,0 +1,51 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      street: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      complement: {
+        type: Sequelize.STRING,
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING(2),
+      },
+      cep: {
+        allowNull: false,
+        type: Sequelize.STRING(8),
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipients');
+  },
+};
