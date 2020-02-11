81162a5e0fca515718a81419c69d9c0bd4b911de
Harlan Cleiton <harlancleiton@gmail.com>
2020-02-11 09:14:39 -0300

    test(server): add before each

--- server/__tests__/integration/me.test.js
+++ server/__tests__/integration/me.test.js
@@ -3,8 +3,13 @@ import factory from '../factory';
 
 import app from '../../src/app';
 import jwtService from '../../src/app/services/JwtService';
+import truncate from '../util/truncate';
 
 describe('Me', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should return the logged in user at the time of request', async () => {
     const user = await factory.create('User', { password: 'admin' });
     const { token } = await jwtService.login(user.email, 'admin');
