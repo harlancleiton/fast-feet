fec7a42cf0f576870fc3e57a416524a62b031a66
Harlan Cleiton <harlancleiton@gmail.com>
2020-03-17 22:59:15 -0300

    chore(web): initial commit

---
+++ clients/web/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/static-property-placement': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': [
+      'error',
+      {
+        allow: ['tron'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
