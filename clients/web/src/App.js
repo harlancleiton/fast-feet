5b36b6d16a76892fc82189df58e291a9865c0093
Harlan Cleiton <harlancleiton@gmail.com>
2020-03-18 00:18:19 -0300

    chore(web): add global styles

--- clients/web/src/App.js
+++ clients/web/src/App.js
@@ -8,12 +8,14 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 import theme from './styles/theme';
+import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
     <Router history={history}>
       <ThemeProvider theme={theme}>
         <Routes />
+        <GlobalStyle />
       </ThemeProvider>
     </Router>
   );
--- clients/web/src/styles/theme.js
+++ clients/web/src/styles/theme.js
@@ -1,5 +1,6 @@
 export default {
   colors: {
     background: '#F5F5F5',
+    white: '#fff',
   },
 };
