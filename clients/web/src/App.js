669ce7ab4d6c2e4ca55f6bdd216f3284e8d8cc9a
Harlan Cleiton <harlancleiton@gmail.com>
2020-03-18 00:09:10 -0300

    chore(web): add default layout

--- clients/web/src/App.js
+++ clients/web/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import 'normalize.css';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
+import theme from './styles/theme';
 
 export default function App() {
   return (
     <Router history={history}>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ clients/web/src/styles/theme.js
@@ -0,0 +1,5 @@
+export default {
+  colors: {
+    background: '#F5F5F5',
+  },
+};
